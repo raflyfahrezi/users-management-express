514dd539ce2baf91af878cfe524a2893e61138d6
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2024-10-22 08:38:13 +0700

    add globals d ts

---
+++ globals.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    DATABASE_PORT: string
+    DATABASE_USER: string
+    DATABASE_HOST: string
+    DATABASE_NAME: string
+    DATABASE_PASSWORD: string
+  }
+}
