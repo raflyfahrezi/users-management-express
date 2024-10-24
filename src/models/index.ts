eec75bc65cf3aca6dcf2bcb50897428a5a0fdc94
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2024-10-24 19:52:32 +0700

    fix models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,8 @@
-import { z } from 'zod'
+import { InferSelectModel } from 'drizzle-orm'
+
+import { usersTable } from '@/database'
+
+export interface IUsers extends InferSelectModel<typeof usersTable> {}
 
 export type TResponseSchema<T> = {
   code: number
