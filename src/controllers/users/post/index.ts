44c23ce28c63ff6cec4bf3df5e591d3b2ccdd656
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2024-10-24 19:52:27 +0700

    fix controllers users post

--- src/controllers/users/post/index.ts
+++ src/controllers/users/post/index.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod'
+import { createInsertSchema } from 'drizzle-zod'
 import { Request, Response } from 'express'
 
 import { responseSchema } from '@/utils'
@@ -7,15 +7,29 @@ import { db, usersTable } from '@/database'
 export const usersPost = async (req: Request, res: Response) => {
   const body = req.body
 
-  //   const response = await db.insert(usersTable).values({
-  //     name: 'Farhan',
-  //     email: 'raflyfahr15@gmail.com',
-  //   })
+  const testBody = createInsertSchema(usersTable)
+  const testResult = testBody.safeParse(body)
+
+  if (!testResult.success) {
+    res.json(
+      responseSchema({
+        code: res.statusCode,
+        data: {},
+        message: 'Input is unprocessable',
+      })
+    )
+
+    return
+  }
+
+  await db.insert(usersTable).values({
+    ...body,
+  })
 
   res.json(
     responseSchema({
-      code: 200,
-      data: {},
+      code: res.statusCode,
+      data: { ...body },
       message: 'Add users success',
     })
   )
