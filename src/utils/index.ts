db1d92e03ec685d1a4cf32f8da7122510b474f7b
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2024-10-25 19:57:12 +0700

    fix utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,7 @@
+import { NextFunction, Request, Response, ErrorRequestHandler } from 'express'
+
 import { TResponseSchema } from '@/models'
+import { STATUS_INTERNAL_SERVER_ERROR } from '@/constants'
 
 export const responseSchema = <T>({
   code,
@@ -10,4 +13,21 @@ export const responseSchema = <T>({
     data,
     message,
   }
+}
+
+export const errorHandler = (
+  err: ErrorRequestHandler,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.statusCode = STATUS_INTERNAL_SERVER_ERROR.code
+  res.json(
+    responseSchema<null>({
+      code: res.statusCode,
+      data: null,
+      message: STATUS_INTERNAL_SERVER_ERROR.message,
+    })
+  )
+  res.end()
 }
