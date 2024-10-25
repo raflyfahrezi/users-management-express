d438db1bca70dfde679a6669d87c3ba1d4815304
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2024-10-25 19:56:16 +0700

    add new status code

--- src/constants/status-code/index.ts
+++ src/constants/status-code/index.ts
@@ -9,3 +9,13 @@ export const STATUS_CONFLICT: TResponseStatus = {
   code: 409,
   message: 'Conflict',
 }
+
+export const STATUS_INTERNAL_SERVER_ERROR: TResponseStatus = {
+  code: 500,
+  message: 'Internal Server Error',
+}
+
+export const STATUS_BAD_REQUEST: TResponseStatus = {
+  code: 400,
+  message: 'Bad Request',
+}
